d05b84e507b863966a04e4e82417d5b3fe52f6b5
Newbyte <stehan253@edu.linkoping.se>
2018-03-27 11:21:34 +0000

    moar code

--- webGL-animation/js/load.js
+++ webGL-animation/js/load.js
@@ -48,3 +48,51 @@ function startup() {
         animateScene();
     
 }
+
+function buildShaderProgram(shaderInfo) {
+    
+    let program = gl.createProgram();
+    
+    shaderInfo.forEach(function(desc) {
+       
+       let shader = compileShader(desc.id, desc.type);
+       
+       if (shader) {
+           
+           gl.attachShader(program, shader);
+           
+       }
+        
+    });
+    
+    gl.linkProgram(program);
+    
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        
+        console.log("Error linking shader program: ");
+        console.log(gl.getProgramInfoLog(program));
+        
+    }
+    
+    return program;
+    
+}
+
+function compileShader(id, type) {
+    
+    let code = document.getElementById(id).firstChild.nodeValue;
+    let shader = gl.createShader(type);
+    
+    gl.shaderSource(shader, code);
+    gl.compileShader(shader);
+    
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        
+        console.log(`Error compiling ${type === gl.VERTEX_SHADER ? "vertex" : "fragment" } shader: `)
+        console.log(gl.getShaderInfoLog(shader));
+        
+    }
+    
+    return shader;
+    
+}
