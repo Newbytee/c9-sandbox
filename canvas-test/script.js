28b1af915b5d37556960f5b8e114ec0c957a231b
Newbyte <stehan253@edu.linkoping.se>
2018-08-23 14:43:32 +0000

    mep

--- canvas-test/script.js
+++ canvas-test/script.js
@@ -8,11 +8,18 @@ const width = 200;
 let boxX = 0;
 let boxY = 100;
 
-setInterval(function() {
+const gameLoop = setInterval(function() {
     ctx.fillStyle = "#FF00FF";
     ctx.fillRect(0, 0, width, height);
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(boxX - 5, boxY - 5, boxX + 5, boxY + 5);
+    ctx.fillRect(boxX - 5, boxY + 5, boxX + 5, boxY - 5);
     boxX++;
-    boxY--;
+    //boxY--;
 }, 200);
+
+document.addEventListener("keypress", function(event) {
+    console.log(event.keyCode);
+    if (event.keyCode === 69) {
+        clearInterval(gameLoop);
+    }
+})
